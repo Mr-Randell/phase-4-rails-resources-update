a5f4806f77e8dac8c527d87cb88b185e81a5ed2c
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-11-23 23:43:56 +0300

    commit tree 6f78ef6be23f11aef946aa50d6e928bba018b0c3

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,38 @@ class BirdsController < ApplicationController
     end
   end
 
+  # PATCH /birds/:id
+  def update
+    # find bird
+    bird = Bird.find_by(id: params[:id])
+
+    # update bird
+    if bird
+      bird.update(bird_params)
+      render json: bird, status: 202
+    else
+      render json: { error: "Bird not found"}, status: 404
+    end
+  end
+
+  # PATCH /birds/:id/like
+  def increment_likes
+    # find the bird first
+    bird = Bird.find_by(id: params[:id])
+
+    # update likes
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird Not Found!"}, status: 404
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
